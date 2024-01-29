37f1b6840e786f3e85448695d36125e09e2e4c3c
marcmillie <mrmarcusmiller0@gmail.com>
2024-01-29 18:41:02 -0500

    Project finished. font resized and moved in place

--- src/App.js
+++ src/App.js
@@ -4,14 +4,22 @@ import './App.css';
 function App() {
   return (
     <div className="App">
-        <img src={logo} className="logo" alt="logo" />
-        
-        <h1>
-         Imrove your front-end skills by building projects
-        </h1>
-        <p className="p">
-          Scan the QR code to visit Frontend Mentor and take your coding skills to the next level
-        </p>
+      <div className="container">
+        <div classname="ctn-one">
+          <img src={logo} className="logo" alt="logo" />
+        </div>
+        <div className='ctn-two'>
+          <h1>
+          Improve your front-end skills by building projects
+          </h1>
+        </div>
+        <div className="ctn-three">
+          <p>
+            Scan the QR code to visit Frontend Mentor
+            and take your coding skills to the next level
+          </p>
+        </div>
+      </div>
     </div>
   );
 }
